6249bcf80ed291a371e9278b0d6a77069cf6e662
Idris adekunle <idrisadekunle726@gmail.com>
2022-05-13 09:03:51 +0100

    added dialog function

--- src/components/Image-Result/Img-result.js
+++ src/components/Image-Result/Img-result.js
@@ -7,6 +7,16 @@ import  Dialog from 'material-ui/Dialog'
 import  FlatButton from 'material-ui/FlatButton'
 
 export class ImageResults extends Component {
+  state ={
+    open:false,
+    currentImg:''
+  }
+  handleOpen= img =>{
+    this.setState({open:true, currentImg:img})
+}
+  handleClose= () =>{
+    this.setState({open:false})
+}
   render() {
 
     let ImageListContent;
@@ -18,7 +28,7 @@ export class ImageResults extends Component {
 {          const{largeImageURL, user, id, tags} = img
 return(
   
-  <GridTile title={tags} key={id} subtitle ={<span> by <strong> {user} </strong> </span>} actionIcon={<IconButton> <ZoomIn color = 'white'/> </IconButton>}>
+  <GridTile title={tags} key={id} subtitle ={<span> by <strong> {user} </strong> </span>} actionIcon={<IconButton onClick={()=>this.handleOpen(largeImageURL)}> <ZoomIn color = 'white'/> </IconButton>}>
 <img src={largeImageURL} alt=''/>
              </GridTile>
 )          })}
@@ -28,10 +38,21 @@ return(
     }
     else{
       ImageListContent = null;
-
     }
+
+    const actions = [
+      <FlatButton label='Close' primary={true} onClick={this.handleClose}/>
+    ]
     return (
-      <div>{ImageListContent}</div>
+      <div>{ImageListContent}
+     <Dialog actions={actions}
+     modal = {false}
+     open = {this.state.open}
+     onRequestClose={this.handleClose} >
+<img src={this.state.currentImg} alt='' style={{ width:'100%'}}/>
+
+     </Dialog>
+      </div>
     )
   }
 }
